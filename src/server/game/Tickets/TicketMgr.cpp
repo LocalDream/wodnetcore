11d52609956d40827471fbdc38849854997134a6
illusion <breakboy91@mail.ru>
2013-05-11 11:19:57 +0200

    Core/Tickets
    * Fixed .ticket complete command not marking tickets as completed
    * Fixed "Need more help" button functionality after ticket has been answered by a GM
    
    Closes #9383

--- src/server/game/Tickets/TicketMgr.cpp
+++ src/server/game/Tickets/TicketMgr.cpp
@@ -248,7 +248,10 @@ void GmTicket::SetChatLog(std::list<uint32> time, std::string const& log)
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 // Ticket manager
-TicketMgr::TicketMgr() : _status(true), _lastTicketId(0), _lastSurveyId(0), _openTicketCount(0), _lastChange(time(NULL)) { }
+TicketMgr::TicketMgr() : _status(true), _lastTicketId(0), _lastSurveyId(0), _openTicketCount(0),
+    _lastChange(time(NULL))
+{
+}
 
 TicketMgr::~TicketMgr()
 {
@@ -256,7 +259,10 @@ TicketMgr::~TicketMgr()
         delete itr->second;
 }
 
-void TicketMgr::Initialize() { SetStatus(sWorld->getBoolConfig(CONFIG_ALLOW_TICKETS)); }
+void TicketMgr::Initialize()
+{
+    SetStatus(sWorld->getBoolConfig(CONFIG_ALLOW_TICKETS));
+}
 
 void TicketMgr::ResetTickets()
 {
