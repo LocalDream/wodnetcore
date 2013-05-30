masterking32/TrinityCore

Core/Players: Cast Summon Pet on first login, relocate created pets
*Casts a proper Summon Pet spell on a hunter logging in for the first
time (depending on his race).
*Relocates created tamed pets before they're added to map.

(cherry picked from commit 090951ba71eef0b27ff6d4c152f79c417f0ab3f0)

src/server/game/Spells/SpellEffects.cpp
@@ -5129,6 +5129,11 @@ void Spell::EffectCreateTamedPet()
     if (!pet)
         return;
 
+    // relocate
+    float px, py, pz;
+    unitTarget->GetClosePoint(px, py, pz, pet->GetCombatReach(), PET_FOLLOW_DIST, pet->GetFollowAngle());
+    pet->Relocate(px, py, pz, unitTarget->GetOrientation());
+
     // add to world
     pet->GetMap()->AddToMap(pet->ToCreature());
 

LocalDream/wodnetcore

src/server/game/Spells/SpellEffects.cpp
@@ -6059,6 +6059,11 @@ void Spell::EffectCreateTamedPet(SpellEffIndex effIndex)
     if (!pet)
         return;
 
+    // relocate
+    float px, py, pz;
+    unitTarget->GetClosePoint(px, py, pz, pet->GetObjectSize(), PET_FOLLOW_DIST, pet->GetFollowAngle());
+    pet->Relocate(px, py, pz, unitTarget->GetOrientation());
+
     // add to world
     pet->GetMap()->AddToMap(pet->ToCreature());
 